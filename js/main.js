2cb58ea90276bd41c07b764285bc0712ea11403a
blenz <bbluegga@yahoo.com>
2019-07-19 18:47:01 -0700

    mkae projects a json file

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-import projects from './data.js';
-
 $('#about-link').click(function(e) {
   e.preventDefault();
   $('html, body').animate(
@@ -30,23 +28,26 @@ $('#contact-link').click(function(e) {
   );
 });
 
-projects.forEach((p, i) => {
-  if (i % 2 === 0) $('#projects').append("<div class='row'></div>");
+// list the projects from projects.json
+$.getJSON('js/projects.json', projects => {
+  projects.forEach((p, i) => {
+    if (i % 2 === 0) $('#projects').append("<div class='row'></div>");
 
-  $('#projects > div.row:last-child').append(
-    `<div class="container col-lg-6">
-      <h3 class="card-title">${p.name}</h5>
-      <img class="card-img-top" 
-        src="${p.img.src}"
-        height="${p.img.height}"
-        width="${p.img.width}"
-      >
-      <p class="text-muted">
-        ${p.body}
-      </p>
-       ${p.tags
-         .map(t => `<span class="label label-primary">${t}</span>`)
-         .join('\n')}
-    </div>`
-  );
+    $('#projects > div.row:last-child').append(
+      `<div class="container col-lg-6">
+        <h3 class="card-title">${p.name}</h5>
+        <img class="card-img-top" 
+          src="${p.img.src}"
+          height="${p.img.height}"
+          width="${p.img.width}"
+        >
+        <p class="text-muted">
+          ${p.body}
+        </p>
+         ${p.tags
+           .map(t => `<span class="label label-primary">${t}</span>`)
+           .join('\n')}
+      </div>`
+    );
+  });
 });
